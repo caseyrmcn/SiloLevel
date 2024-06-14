8a08fb0da9c59913f5f4812b2c6fd6e468d79472
Casey McNaughton <82733871+caseyrmcn@users.noreply.github.com>
2024-06-14 00:12:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('form-square').addEventListener('submit', function(event
     // Get the value from the first input field
     const inputValue1 = parseFloat(document.getElementById('inputValue1').value);
 
-    // Calculate the square of the input value
+    // Calculate the level of silo 9 & 10
     const result1 = inputValue1 * inputValue1;
 
     // Display the result in the first result paragraph
@@ -14,14 +14,14 @@ document.getElementById('form-square').addEventListener('submit', function(event
 });
 
 // Add an event listener to the second form to handle the submit event
-document.getElementById('form-double').addEventListener('submit', function(event) {
+document.getElementById('form-silo11').addEventListener('submit', function(event) {
     // Prevent the default form submission behavior
     event.preventDefault();
 
     // Get the value from the second input field
     const inputValue2 = parseFloat(document.getElementById('inputValue2').value);
 
-    // Calculate the double of the input value
+    // Calculate the level of silo 11 & 12
     const result2 = inputValue2 * 2;
 
     // Display the result in the second result paragraph
