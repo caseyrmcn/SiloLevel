8aa3825ea0f4228e6a1a8a5936ae2465d2b2b1e8
Casey McNaughton <82733871+caseyrmcn@users.noreply.github.com>
2024-08-28 04:10:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function calculateSilo9(ft, inches) {
     // Multiply material bulk density by Volume of material
     const siloLb = totalMaterialFt * 40
 
-    const siloNine = siloLb + 7522
+    const siloNine = siloLb + 7051
 
     return siloNine ;
 }
@@ -35,33 +35,47 @@ function calculateSilo11(ft, inches) {
     // Fix measurement with height offset
     const siloHeightOne = totalInches - 14;
 
-    // Take measurement and subtract from overall silo height in IN
-    const siloHeight = 160 - siloHeightOne ;
-
     // Take material height and convert to cubed IN
-    const matVolumeIn = 15386 * siloHeight;
+    if (siloHeightOne > 159) {
+        coneHeight = 220 - siloHeightOne;
+	coneHeightPercent = coneHeight / 60 
+	coneRadius = 70 * coneHeightPercent
+	coneRadiusSquared = coneRadius * coneRadius
+	coneRadiusTotal = 0.33 * 3.14 * coneRadiusSquared * coneHeight;
+	coneTotal = coneRadiusTotal / 1728 * 43
+	return coneTotal;
+        } else {
+	    siloHeight = 160 - siloHeightOne ;
+    }
 
-    // Add cone volume to material volume
-    //const totalMaterialVol = matVolumeIn + 307876;
+
+    // Take measurement and subtract from overall silo height in IN
+    //const siloHeight = 160 - siloHeightOne ;
+
+    const matVolumeIn = 15386 * siloHeight;
 
     // Convert cubed IN to Cuber FT
     const totalMaterialFt = matVolumeIn / 1728;
 
 
     // Multiply material bulk density by Volume of material
-    const siloLb = totalMaterialFt * 42
+    const siloLb = totalMaterialFt * 43
 
     const siloEleven = siloLb + 7580
 
-    if (siloEleven > 49000) {
-	total = siloEleven - 3500;
-    } else if (siloEleven > 39000) {
-	total = siloEleven - 1700;
+    //NE silo adjustment 
+    if (siloEleven > 59000) {
+	total = siloEleven - 3000;
+    } else if (siloEleven > 49000) {
+	total = siloEleven - 2000;
+    }else if (siloEleven > 39000) {
+	total = siloEleven - 1000;
     } else {
 	total = siloEleven;
     }
     
     return total;
+   
 }
 
 // Event listener for Silo 9 form submission
